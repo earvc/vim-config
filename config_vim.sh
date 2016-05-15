dce1827c55b88689d36dda465e9aff717b030883
Earvin <earvinrc@gmail.com>
2016-05-14 20:22:54 -0400

    Updated directory name in config script

--- config_vim.sh
+++ config_vim.sh
@@ -2,6 +2,6 @@
 rm -rf ~/.vim > /dev/null
 rm ~/.vimrc > /dev/null
 echo "Setting up symlinks to .vim directory and .vimrc config file"
-ln -s ~/Documents/vim_config/vim ~/.vim
-ln -s ~/Documents/vim_config/vimrc ~/.vimrc
+ln -s ~/Documents/vim-config/vim ~/.vim
+ln -s ~/Documents/vim-config/vimrc ~/.vimrc
 echo "Done!"
