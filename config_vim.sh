61e1f1ae78e4f568955a37069a0acb6ab51e8912
Earvin Caceres <earvinrc@gmail.com>
2020-01-11 21:09:38 -0800

    Added tmux.conf file

--- config_vim.sh
+++ config_vim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -rf ~/.vim > /dev/null
 rm ~/.vimrc > /dev/null
-echo "Setting up symlinks to .vim directory and .vimrc config file"
+echo "Setting up symlinks to dot files"
 ln -s ~/Documents/vim-config/vim ~/.vim
 ln -s ~/Documents/vim-config/vimrc ~/.vimrc
+ln -s ~/Documents/vim-config/tmux.conf ~/.tmux.conf
 echo "Done!"
