d5438b3442db4b5937844c1acb940b82ddc0ba10
Earvin Caceres <earvinrc@gmail.com>
2020-03-07 14:07:53 -0800

    Removed vim dir symlink

--- config_vim.sh
+++ config_vim.sh
@@ -2,7 +2,6 @@
 rm -rf ~/.vim > /dev/null
 rm ~/.vimrc > /dev/null
 echo "Setting up symlinks to dot files"
-ln -s ~/vim-config/vim ~/.vim
 ln -s ~/vim-config/vimrc ~/.vimrc
 ln -s ~/vim-config/tmux.conf ~/.tmux.conf
 echo "Done!"
