f468113395677ca30ef85bceae39ff54e9c74957
Earvin Caceres <earvinrc@gmail.com>
2020-06-14 21:37:12 -0700

    Modified config script to install plugin dependencies and added some nice prints.

--- config_vim.sh
+++ config_vim.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-rm -rf ~/.vim > /dev/null
-rm ~/.vimrc > /dev/null
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+rm -rf ~/.vim > /dev/null 2>&1
+rm ~/.vimrc > /dev/null 2>&1
+rm ~/.tmux.conf > /dev/null 2>&1
+
+echo "Installing dependencies"
+echo "======================="
+echo "--> Curl"
+apt-get install curl
+echo ""
+
+echo "--> Ack"
+apt-get install ack
+echo ""
+
 echo "Setting up symlinks to dot files"
-ln -s ~/vim-config/vimrc ~/.vimrc
-ln -s ~/vim-config/tmux.conf ~/.tmux.conf
+echo "================================"
+# Execute in sub-shells to print commands
+(set -x; ln -s ~/vim-config/vimrc ~/.vimrc)
+(set -x; ln -s ~/vim-config/tmux.conf ~/.tmux.conf)
+
+echo ""
 echo "Done!"
